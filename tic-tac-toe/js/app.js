7602cc996692b709068a7050635353c4dd951935
Alina Gonzalez <alinajg24@gmail.com>
2020-02-09 18:19:00 -0500

    ok dokes

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -14,6 +14,9 @@ const winningConditions = [
 let board;
 let turn;
 let win;
+let gameStarted = false;
+let scoreX = 0;
+let scoreO = 0;
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
@@ -25,6 +28,11 @@ const message = document.querySelector("h2");   // grab the subheader
 window.onload = init;
 document.getElementById("board").onclick = takeTurn;
 document.getElementById("reset-button").onclick = init;
+document.getElementById("score-reset").onclick = resetScore;
+document.getElementById("player-scoreX").innerHTML = scoreX;
+document.getElementById("player-scoreO").innerHTML = scoreO;
+document.getElementById('buttonX').addEventListener('buttonX', startPlayer);
+document.getElementById('buttonO').addEventListener('buttonO', startPlayer);
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
@@ -35,9 +43,11 @@ function init() {
     "", "", ""
   ];
 
-  turn = "X";
+  turn = "";
   win = null;
-
+  gameStarted = false;
+  document.getElementById("buttonO").classList.remove("selected");
+  document.getElementById("buttonX").classList.remove("selected");
   render();   // we'll write this later
 }
 function render() {
@@ -45,10 +55,19 @@ function render() {
     squares[index].textContent = mark;
   });
 
+  if (win == "X"){
+    scoreX = scoreX + 1;
+    document.getElementById("player-scoreX").innerHTML = scoreX;
+  } else if (win == "O"){
+    scoreO = scoreO + 1;
+    document.getElementById("player-scoreO").innerHTML = scoreO;
+  }
+
   message.textContent =
     win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
 }
 function takeTurn(e) {
+  gameStarted = true;
   if (!win) {
     let index = squares.findIndex(function(square) {
       return square === e.target;
@@ -77,4 +96,26 @@ function getWinner() {
   });
 
   return winner ? winner : board.includes("") ? null : "T";
+}
+function startPlayer(e){
+  if (gameStarted) return;
+
+  if (e.id == "buttonX"){
+    turn = "X";
+    document.getElementById("turn-header").innerHTML = "Turn: X";
+    document.getElementById("buttonO").classList.remove("selected");
+    document.getElementById(e.id).classList.add("selected");
+  }else{
+    turn = "O";
+    document.getElementById(e.id).classList.add("selected");
+    document.getElementById("buttonX").classList.remove("selected");
+    document.getElementById("turn-header").innerHTML = "Turn: O";
+  }
+}
+function resetScore() {
+  init();
+  scoreX = 0;
+  scoreO = 0;
+  document.getElementById("player-scoreX").innerHTML = scoreX;
+  document.getElementById("player-scoreO").innerHTML = scoreO;
 }
